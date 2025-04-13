a44b2cbc96adc5807048e3e3956252d482085992
Przemek Lekston <p.lekston@gmail.com>
2025-04-13 20:14:10 +0200

    Working Dockerfiles for kiss-icp and lio-sam

--- ros_on_docker_nvidia/run_ros2_docker_gpu.sh
+++ ros_on_docker_nvidia/run_ros2_docker_gpu.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Build the Docker image if not already built
-docker build -t ros2-dev-nvidia .
+docker build -t ros2-dev-gpu-s1 -f Dockerfile_ros_gpu_base .
+docker build -t ros2-dev-gpu-kiss-icp -f Dockerfile_ros_kiss_icp ../..
+docker build -t ros2-dev-gpu-liosam -f Dockerfile_ros_lio_sam .
 
 # Run the container with X11 forwarding and NVIDIA GPU access
 docker run -it --rm \
@@ -9,6 +11,8 @@ docker run -it --rm \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="`pwd`:/ros2_ws/src_wip" \
+    --volume="/opt/mnt/data/10_slam/:/ros2_ws/data/" \
     --network=host \
     --name ros2-dev-gpu-container \
-    ros2-dev-nvidia 
+    ros2-dev-gpu-liosam
