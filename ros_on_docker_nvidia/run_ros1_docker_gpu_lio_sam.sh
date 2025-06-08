1a15fda6badb7c0a2e3dcdb9ffaf5e76153f561c
Przemek Lekston <p.lekston@gmail.com>
2025-06-09 00:03:26 +0200

    Fix GPU-enabled docker for LIO-SAM

--- ros_on_docker_nvidia/run_ros1_docker_gpu_lio_sam.sh
+++ ros_on_docker_nvidia/run_ros1_docker_gpu_lio_sam.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Build the Docker image if not already built
-docker build -t ros1-dev-gpu-base -f Dockerfile_ros1_gpu_base .
-# docker build -t ros1-dev-gpu-liosam -f Dockerfile_ros1_gpu_lio_sam .
+docker build -t ros1-dev-gpu-base -f ros_on_docker_nvidia/Dockerfile_ros1_gpu_base .
+docker build -t ros1-dev-gpu-liosam -f ros_on_docker_nvidia/Dockerfile_ros1_gpu_lio_sam .
 
 # Run the container with X11 forwarding and NVIDIA GPU access
 docker run -it --rm \
@@ -14,4 +14,4 @@ docker run -it --rm \
     --volume="/opt/mnt/data/10_slam/:/data/" \
     --network=host \
     --name ros1-dev-gpu-container \
-    ros1-dev-gpu-base
+    ros1-dev-gpu-liosam
